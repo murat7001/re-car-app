b91836c825ed20e729f9f6c7131a367485225935
Murat Yıldırım <2020555068@ogr.cu.edu.tr>
2023-12-13 14:10:08 +0300

    Add page title and favicon.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <title>Rent A Car</title>
+      <link href="/transparan.png" rel="shortcut icon" type="image/png" sizes="16x16" />
+      </Head>
       <body>
         <Main />
         <NextScript />
