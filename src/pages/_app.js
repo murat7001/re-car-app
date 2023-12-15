b4c9b3a1681f53b44ec117c534fb85dce7bb55bc
Murat Yıldırım <2020555068@ogr.cu.edu.tr>
2023-12-16 02:50:50 +0300

    Add Material UI theme provider and custom theme.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import { store } from '@/store/store';
 import { useState, useEffect } from 'react';
 import Head from 'next/head';
+import { ThemeProvider } from '@mui/material';
+import theme from '@/styles/theme';
 
 export default function App({ Component, pageProps }) {
   const [isClient, setIsClient] = useState(false);
@@ -21,12 +23,15 @@ export default function App({ Component, pageProps }) {
       <Head>
         <title>Rent A Car</title>
         <link href="/transparan.png" rel="shortcut icon" type="image/png" sizes="32x32" />
+        <link href="https://fonts.googleapis.com/css2?family=Brawler:wght@700&display=swap" rel="stylesheet"/>
       </Head>
-      <Provider store={store}>
-        <DashBoard>
-          <Component {...pageProps} />
-        </DashBoard>
-      </Provider>
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <DashBoard>
+            <Component {...pageProps} />
+          </DashBoard>
+        </Provider>
+      </ThemeProvider>
     </>
 
   );
