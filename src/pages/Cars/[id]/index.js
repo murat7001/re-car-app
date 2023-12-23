8a1aeae2e03109d8e6bff01bb54cfff1eccb2067
Murat Yıldırım <2020555068@ogr.cu.edu.tr>
2023-12-23 18:11:02 +0300

    Update Cars/[id] and Home pages to handle loading state

--- src/pages/Cars/[id]/index.js
+++ src/pages/Cars/[id]/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Stack, Box, Typography, Button } from '@mui/material';
+import { Stack, Box, Typography, Button, CircularProgress } from '@mui/material';
 import { useRouter } from 'next/router';
 import { useDispatch, useSelector } from 'react-redux';
 import ImageGallery from 'react-image-gallery';
@@ -9,19 +9,14 @@ const Details = () => {
     const router = useRouter();
     const { id } = router.query;
     const dispatch = useDispatch();
-    const { carsDetails } = useSelector((state) => state.cars);
+    const { carsDetails, loadingCarsDetails } = useSelector((state) => state.cars);
     let { loggedIn, user } = useSelector((state) => state.auth);
-
 
     useEffect(() => {
         if (id) {
             dispatch(fetchCarDetails(id));
         }
     }, [id, dispatch]);
-
-    if (!carsDetails) {
-        return <div>Loading...</div>;
-    }
 
     const images = carsDetails.photos ? carsDetails.photos.map((url) => ({ original: url })) : [];
 
@@ -32,6 +27,7 @@ const Details = () => {
             router.push('/Auth/Signin');
         }
     };
+
     return (
         <Stack
             sx={{
@@ -42,54 +38,63 @@ const Details = () => {
                 justifyContent: 'center',
             }}
         >
-            <Box
-                sx={{
-                    maxWidth: '900px',
-                    width: '100%',
-                    display: 'flex',
-                    justifyContent: 'space-around',
-                    background: '#F3F3F3',
-                    padding: '50px',
-                    borderRadius: '30px'
-                }}
-            >
-                <Box sx={{
-                    height: '300px',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                }}>
-                    <ImageGallery showPlayButton={false} items={images} />
+            {loadingCarsDetails ? (
+                <CircularProgress style={{ margin: 'auto', display: 'block' }} />
+            ) : (
+                <Box
+                    sx={{
+                        maxWidth: '900px',
+                        width: '100%',
+                        display: 'flex',
+                        justifyContent: 'space-around',
+                        background: '#F3F3F3',
+                        padding: '50px',
+                        borderRadius: '30px',
+                    }}
+                >
+                    <Box
+                        sx={{
+                            height: '300px',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <ImageGallery showPlayButton={false} items={images} />
+                    </Box>
+                    <Stack>
+                        <Typography variant="h4">{carsDetails.name}</Typography>
+                        <Typography
+                            sx={{ borderBottom: '1px solid #ccc', paddingBottom: '1px', marginTop: '20px' }}
+                        >
+                            Fuel Type: {carsDetails.fuelType}
+                        </Typography>
+                        <Typography
+                            sx={{ borderBottom: '1px solid #ccc', paddingBottom: '1px', marginTop: '15px' }}
+                        >
+                            Type: {carsDetails.transmissionType}
+                        </Typography>
+                        <Typography
+                            sx={{ borderBottom: '1px solid #ccc', paddingBottom: '1px', marginTop: '15px' }}
+                        >
+                            Price per day: {carsDetails.pricePerDay} TL
+                        </Typography>
+                        <Typography
+                            sx={{ borderBottom: '1px solid #ccc', paddingBottom: '1px', marginTop: '15px' }}
+                        >
+                            Capacity: {carsDetails.capacity}
+                        </Typography>
+
+                        {user?.role !== 'admin' && (
+                            <Stack sx={{ mt: 'auto' }}>
+                                <Button onClick={handleButtonClick} variant="contained">
+                                    Rent
+                                </Button>
+                            </Stack>
+                        )}
+                    </Stack>
                 </Box>
-                <Stack>
-                    <Typography variant="h4">
-                        {carsDetails.name}
-                    </Typography>
-                    <Typography sx={{ borderBottom: '1px solid #ccc', paddingBottom: '1px', marginTop: '20px' }}>
-                        Fuel Type: {carsDetails.fuelType}
-                    </Typography>
-                    <Typography sx={{ borderBottom: '1px solid #ccc', paddingBottom: '1px', marginTop: '15px' }}>
-                        Type: {carsDetails.transmissionType}
-                    </Typography>
-                    <Typography sx={{ borderBottom: '1px solid #ccc', paddingBottom: '1px', marginTop: '15px' }}>
-                        Price per day: {carsDetails.pricePerDay} TL
-                    </Typography>
-                    <Typography sx={{ borderBottom: '1px solid #ccc', paddingBottom: '1px', marginTop: '15px' }}>
-                        Capacity: {carsDetails.capacity}
-                    </Typography>
-
-
-                    {user?.role !== 'admin' && (
-                        <Stack sx={{ mt: 'auto' }}>
-                            <Button onClick={handleButtonClick} variant='contained'>Rent</Button>
-                        </Stack>
-                    )}
-
-
-                </Stack>
-
-
-            </Box>
+            )}
         </Stack>
     );
 };
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,13 +4,14 @@ import { fetchCarByCategory, fetchCars } from "@/store/CarSlice/carSlice";
 import {
     Grid,
     Stack,
+    CircularProgress,
 } from '@mui/material';
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 const Home = () => {
     const dispatch = useDispatch();
-    const { cars } = useSelector((state) => state.cars);
+    const { cars, loadingCars } = useSelector((state) => state.cars);
     const { selectedCategory } = useSelector((state) => state.categories);
 
     useEffect(() => {
@@ -26,24 +27,28 @@ const Home = () => {
         <div>
             <Navbar></Navbar>
 
-            <Stack
-                width={"90%"}
-                margin={"auto"}
-                marginTop={3}
-                spacing={3}
-                direction={"row"}
-                justifyContent="space-around"
-                useFlexGap
-            >
-                <Grid container spacing={3}>
-                    {cars &&
-                        cars.map((car) => (
-                            <Grid key={car.id} item xs={12} sm={6} md={4} lg={3}>
-                                <CardCmp car={car}></CardCmp>
-                            </Grid>
-                        ))}
-                </Grid>
-            </Stack>
+            {loadingCars ? (
+                <CircularProgress style={{ margin: 'auto', display: 'block', marginTop: '20vh' }} />
+            ) : (
+                <Stack
+                    width={"90%"}
+                    margin={"auto"}
+                    marginTop={3}
+                    spacing={3}
+                    direction={"row"}
+                    justifyContent="space-around"
+                    useFlexGap
+                >
+                    <Grid container spacing={3}>
+                        {cars &&
+                            cars.map((car) => (
+                                <Grid key={car.id} item xs={12} sm={6} md={4} lg={3}>
+                                    <CardCmp car={car}></CardCmp>
+                                </Grid>
+                            ))}
+                    </Grid>
+                </Stack>
+            )}
         </div>
     )
 }
